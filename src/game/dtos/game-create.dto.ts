a1f28819cfae106cb41fe03b52dbcaf8a1c4729c
saleksanyan <92786852+saleksanyan@users.noreply.github.com>
2024-06-04 15:50:30 +0400

    Delete src/game/dtos directory

--- src/game/dtos/game-create.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
-
-export class GameCreateDto {
-  
-  @ApiProperty()
-  @IsNotEmpty()
-    gameToken: string;
-}
