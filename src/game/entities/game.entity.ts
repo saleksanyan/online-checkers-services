36f0edec08b3578ebadb3491d79c8a50055629a5
saleksanyan <92786852+saleksanyan@users.noreply.github.com>
2024-06-05 17:48:18 +0400

    Update game.entity.ts

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -6,9 +6,6 @@ import {
 	PrimaryGeneratedColumn,
 	ValueTransformer,
 } from 'typeorm';
-=======
-import { Entity, Column, Index, PrimaryGeneratedColumn, ValueTransformer } from 'typeorm';
->>>>>>> main
 import Game from 'src/lib/Game';
 import Board from 'src/lib/Board';
 import History from 'src/lib/History';
@@ -21,32 +18,8 @@ function reviveInstance<T>(cls: new (...args: any[]) => T, json: any): T {
 	return Object.assign(new cls(), json);
 }
 const gameTransformer: ValueTransformer = {
-<<<<<<< addedAPI
     to: (game: Game) => serializeGame(game),
     from: (value: string) => deserializeGame(value),
-=======
-	to: (game: Game) => JSON.stringify(game),
-	from: (value: string) => {
-		const gamefromJson: Game = Game.fromJSON(JSON.parse(value));
-		gamefromJson.board = Board.fromJSON(gamefromJson.board);
-    
-    gamefromJson.board.history = History.fromJSON(gamefromJson.board.history);
-		const boardMatrix = gamefromJson.board.getBoard();
-    
-		for (let row = 0; row < boardMatrix.length; row++) {
-			for (let column = 0; column < boardMatrix[row].length; column++) {
-				if (boardMatrix[row][column] !== Color.EMPTY_PLACE) {
-					if ((boardMatrix[row][column] as Figure).figureType === FigureType.PAWN) {
-            boardMatrix[row][column] = Pawn.fromJSON(boardMatrix[row][column]);
-          } else if ((boardMatrix[row][column] as Figure).figureType === FigureType.PAWN) {
-            boardMatrix[row][column] = Queen.fromJSON(boardMatrix[row][column]);
-          }
-				}
-			}
-		}
-		return gamefromJson;
-	},
->>>>>>> main
 };
 
 export default gameTransformer;
